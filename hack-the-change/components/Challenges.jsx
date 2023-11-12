e2da3d0bf733d2e4b1d3dd105afed8bf9770b390
ericli3690 <ericli3690@gmail.com>
2023-11-12 11:02:10 -0700

    basic map working

--- hack-the-change/components/Challenges.jsx
+++ hack-the-change/components/Challenges.jsx
@@ -13,6 +13,10 @@ const Challenges = () => {
         name: "Try Growing Thyme!",
         plant: "Thyme"
       }} />
+      <div>
+        <h3 className="text-center font-semibold mb-2 text-xl" >Nearby Plant Stores</h3>
+        <div className="rounded-md border-8 border-stone-300" style={{overflow:"hidden",resize:"none",maxWidth:"100%",height:"500px"}}><div id="g-mapdisplay" style={{height:"100%", width:"100%", border: 0}}><iframe style={{height:"100%", width:"100%", maxWidth:"100%"}} frameborder="0" src="https://www.google.com/maps/embed/v1/search?q=plant+stores&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="google-maps-html" href="https://kbj9qpmy.com/bp" id="make-map-data">Internet Provider</a></div>
+      </div>
     </>
   )
 }
