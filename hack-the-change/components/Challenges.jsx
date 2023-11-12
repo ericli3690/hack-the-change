436f13c0475ae2b7120b6852eefa57adbdc2a5e0
ericli3690 <ericli3690@gmail.com>
2023-11-12 10:40:49 -0700

    mistake

--- hack-the-change/components/Challenges.jsx
+++ hack-the-change/components/Challenges.jsx
@@ -6,10 +6,12 @@ const Challenges = () => {
     <>
       <h2 className="text-center text-xl font-semibold mb-2">Challenges</h2>
       <SuggestionCard canSwap={false} plantData={{
-        name: "Try Growing Cucumber!"
+        name: "Try Growing Cucumber!",
+        plant: "Cucumber"
       }} />
       <SuggestionCard canSwap={false} plantData={{
-        name: "Try Growing Thyme!"
+        name: "Try Growing Thyme!",
+        plant: "Thyme"
       }} />
     </>
   )
